26c1398084bfd76bc2a82b2d8827a5b60f52539e
Anthony Olivence <anthony.olivence@gmail.com>
2020-10-10 01:00:43 -0400

    Add a metric

--- src/world/world.js
+++ src/world/world.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Globe from 'react-globe.gl';
 import * as d3 from 'd3';
-import { request, getCoordinates, numberWithCommas, formatDate } from '../utils';
 import './world.css';
 
 const flagEndpoint = 'https://corona.lmao.ninja/assets/img/flags';
@@ -12,35 +11,39 @@ const getVal = (feat) => {
   return feat.properties.POP_EST / 7800000000;
 };
 
-const World = ({ countryFeatureData, countryMetricData }) => {
+const getLabel = () => {
+
+}
+const World = ({ countryFeatureData, countryMetricData, dates, currentDate,
+  selectedMetric, onUpdateCountryFeatureData, pointOfView }) => {
   const globeEl = useRef();
   const [hoverD, setHoverD] = useState();
   const [transitionDuration, setTransitionDuration] = useState(1000);
-
 
   useEffect(() => {
     setTimeout(() => {
       setTransitionDuration(4000);
     });
   }, [countryFeatureData.length]);
-    
 
-  useEffect(() => {
-    // Auto-rotate
-    globeEl.current.controls().autoRotate = true;
-    globeEl.current.controls().autoRotateSpeed = 0.3;
-    globeEl.current.pointOfView({ altitude: 4 }, 5000);
-  }, []);
 
+  // useEffect(() => {
+  //   // Auto-rotate
+  //   globeEl.current.controls().autoRotate = true;
+  //   globeEl.current.controls().autoRotateSpeed = 0.3;
+  //   globeEl.current.pointOfView({ altitude: 4 }, 5000);
+  // }, []);
   
+
   return <Globe
     ref={globeEl}
     globeImageUrl="https://cdn.jsdelivr.net/npm/three-globe/example/img/earth-night.jpg"
+    //pointOfView={pointOfView}
     polygonsData={countryFeatureData.features}
     polygonAltitude={d => d === hoverD ? 0.12 : 0.06}
     polygonCapColor={d => d === hoverD ? 'steelblue' : colorScale(getVal(d))}
     polygonSideColor={() => 'rgba(0, 100, 0, 0.15)'}
-    polygonLabel={({ properties: d, countryData: c }) => {
+    polygonLabel={({ properties: d, metricData: metricData }) => {
       if (d.ADMIN === 'France') {
         flagName = 'fr';
       } else if (d.ADMIN === 'Norway') {
@@ -48,6 +51,9 @@ const World = ({ countryFeatureData, countryMetricData }) => {
       } else {
         flagName = d.ISO_A2.toLowerCase();
       }
+      
+      let metric = metricData.find( m => m.metricName === selectedMetric);
+      const metricVal = metric.metricValue;
       return `
         <div class="card">
           <img class="card-img" src="${flagEndpoint}/${flagName}.png" alt="flag" />
@@ -57,9 +63,8 @@ const World = ({ countryFeatureData, countryMetricData }) => {
              <hr />
              <div class="card-spacer"></div>
              <span>Population: ${d3.format('.3s')(d.POP_EST)}</span>
-          </div>
-        </div>
-      `;
+             <span>${selectedMetric}: ${metricVal}</span>
+        `;
     }}
     onPolygonHover={setHoverD}
     polygonsTransitionDuration={transitionDuration}
--- src/worldDashboard/worldDashboard.js
+++ src/worldDashboard/worldDashboard.js
@@ -1,42 +1,120 @@
 import { request, getCoordinates, numberWithCommas, formatDate } from '../utils';
 import React, { useState, useEffect, useRef } from 'react';
 import './worldDashboard.css';
+import * as d3 from 'd3';
 import World from '../world/world';
 
 const COUNTRY_FEATURE_DATA = 'http://localhost:3000/datasets/ne_110m_admin_0_countries.geojson';
-const COUNTRY_METRIC_DATA = 'http://localhost:3000/datasets/ne_110m_admin_0_countries.geojson';
+const COUNTRY_METRIC_DATA = 'http://localhost:3000/datasets/metricData.json';
+const colorScale = d3.scaleSequentialPow(d3.interpolateYlOrRd).exponent(1 / 4);
+const getVal = (feat) => {
+  return feat.properties.POP_EST / 7800000000;
+};
 
 const WorldDashboard = () => {
 
-    const [countryFeatureData, setCountryFeatureData] = useState({ features: [] });
-    const [countryMetricData, setCountryMetricData] = useState([]);
-    const [dataLoading, setDataLoading] = useState(true);
+  const [countryFeatureData, setCountryFeatureData] = useState({ features: [] });
+  const [countryMetricData, setCountryMetricData] = useState([]);
+  const [dataLoading, setDataLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchData = async () => {
-          const countryData = await request(COUNTRY_FEATURE_DATA);
-          const countryMetricData = await request(COUNTRY_METRIC_DATA);
-          setCountryFeatureData(countryData);
-          setCountryMetricData(countryMetricData);
-          setDataLoading(false);
-        };
-      
-        fetchData();
-      }, []);
+  const [currentDate, setCurrentDate] = useState();
+  const [dates, setDates] = useState([]);
+  const [sliderVal, setSliderVal] = useState(0);
+  const [sliderMax, setSliderMax] = useState(0);
+  const [sliderDisabled, setSliderDisabled] = useState('');
+  const [pointOfView, setPointofView] = useState();
 
-    return (
-      <>
-        <div className="top-info-container">
-          <div className="title">Country Metrics</div>
-          <div className="title-desc">
-            { dataLoading ? 'Loading country metric data...' : 'Hover on a country or territory to see risk metrics'}
-          </div>
+  useEffect(() => {
+    const fetchData = async () => {
+      const countryData = await request(COUNTRY_FEATURE_DATA);
+      const countryMetricData = await request(COUNTRY_METRIC_DATA);
+      setCountryFeatureData(countryData);
+      setCountryMetricData(countryMetricData);
+      setDataLoading(false);
+
+      // extract dates
+      const dates = Object.keys(countryMetricData.China);
+      setDates(Object.keys(countryMetricData.China));
+      setCurrentDate(dates.length - 1);
+      // update slider
+      setSliderMax(dates.length - 1);
+      setSliderVal(dates.length - 1);
+      setSliderDisabled('disabled');
+    };
+
+    const updatePointOfView = async () => {
+      // Get coordinates
+      try {
+        const { latitude, longitude } = await getCoordinates();
+
+        setPointofView(
+          {
+            lat: latitude,
+            lng: longitude,
+          },
+          1000
+        );
+      } catch (e) {
+        console.log('Unable to set point of view.');
+      }
+    }
+    fetchData();
+    updatePointOfView();
+  }, []);
+
+  useEffect(() => {
+    if(!currentDate) {
+      return;
+    }
+    for (let x = 0; x < countryFeatureData.features.length; x++) {
+      const country = countryFeatureData.features[x].properties.NAME;
+      if (countryMetricData[country]) {
+        countryFeatureData.features[x].metricData = 
+          countryMetricData[country][dates[currentDate]]
+      } else {
+        countryFeatureData.features[x].metricData = [
+          {
+            metricName: 'tre',
+            metricValue: 0
+          }
+        ]
+      }
+    }
+  
+    const maxVal = Math.max(...countryFeatureData.features.map(getVal));
+    colorScale.domain([0, maxVal]);
+    setCountryFeatureData(countryFeatureData);
+  }, [currentDate]);
+
+  return (
+    <>
+      <div className="top-info-container">
+        <div className="title">Country Metrics</div>
+        <div className="title-desc">
+          {dataLoading ? 'Loading country metric data...' : 'Hover on a country or territory to see risk metrics'}
         </div>
-        <World 
-          countryFeatureData={countryFeatureData}
-          countryMetricData={countryMetricData} />
-      </>
-    ); 
+      </div>
+      <World
+        countryFeatureData={countryFeatureData}
+        countryMetricData={countryMetricData}
+        currentDate={currentDate}
+        dates={dates}
+        onUpdateCountryFeatureData={setCountryFeatureData}
+        pointOfView={pointOfView}
+        selectedMetric={'tre'}
+      />
+      <input
+        className="slider"
+        disabled={sliderDisabled}
+        type="range"
+        min="0"
+        max={sliderMax}
+        step="1"
+        onChange={() => setCurrentDate(sliderVal)}
+      />
+      {/* <p>as of {formatDate(dates[sliderVal])}</p> */}
+    </>
+  );
 
 
 }
